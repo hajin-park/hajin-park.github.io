ab16ec102324397879fc6e250a8cd76f63c237a1
Hajin Park <hajin1819@gmail.com>
2022-10-27 22:17:36 -0700

    |Added title animations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 export default function Footer() {
     return (
         <section className='group relative'>
-            <div className='transition-all delay-100 absolute -translate-x-2 translate-y-2 group-hover:translate-y-3 group-hover:-translate-x-3 -z-10 h-20 w-full bg-green-400'>
+            <div className='transition-all delay-100 absolute -translate-x-2 translate-y-2 group-hover:translate-y-4 group-hover:-translate-x-4 -z-10 h-20 w-full bg-green-400'>
             </div>
-            <div className='transition-all absolute -translate-x-4 translate-y-4 group-hover:translate-y-5 group-hover:-translate-x-5 -z-20 h-20 w-full bg-green-300 shadow-lg'>
+            <div className='transition-all absolute -translate-x-4 translate-y-4 group-hover:translate-y-6 group-hover:-translate-x-6 -z-20 h-20 w-full bg-green-300 shadow-lg'>
             </div>
             <address id='footer' className='transition-all group-active:-translate-x-1 group-active:translate-y-1 bg-green-900 h-20 w-full'>
                 <ul className='h-full w-full flex items-center justify-around font-extrabold text-2xl text-white'>
