def7be9b43a38d27c1229c568da97a04867bcded
Hajin Park <hajin1819@gmail.com>
2022-10-27 22:12:43 -0700

    Added footer and nav bar animations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
 export default function Footer() {
     return (
-        <section className='relative'>
-            <div className='absolute translate-x-2 translate-y-2 -z-10 h-20 w-full bg-green-400'>
+        <section className='group relative'>
+            <div className='transition-all delay-100 absolute -translate-x-2 translate-y-2 group-hover:translate-y-3 group-hover:-translate-x-3 -z-10 h-20 w-full bg-green-400'>
             </div>
-            <div className='absolute translate-x-4 translate-y-4 -z-20 h-20 w-full bg-green-300'>
+            <div className='transition-all absolute -translate-x-4 translate-y-4 group-hover:translate-y-5 group-hover:-translate-x-5 -z-20 h-20 w-full bg-green-300 shadow-lg'>
             </div>
-            <address id='footer' className='bg-green-900 h-20 w-full'>
+            <address id='footer' className='transition-all group-active:-translate-x-1 group-active:translate-y-1 bg-green-900 h-20 w-full'>
                 <ul className='h-full w-full flex items-center justify-around font-extrabold text-2xl text-white'>
-                    <li className=''><a href='https://github.com/hajin-park' target='_blank'>Github</a></li>
-                    <li className=''><a href='https://www.linkedin.com/in/hajinpark/' target='_blank'>LinkedIn</a></li>
-                    <li className=''><a href='https://www.facebook.com/hajinprk' target='_blank'>Facebook</a></li>
+                    <li className=''><a href='https://github.com/hajin-park' target='_blank' rel="noreferrer">Github</a></li>
+                    <li className=''><a href='https://www.linkedin.com/in/hajinpark/' target='_blank' rel="noreferrer">LinkedIn</a></li>
+                    <li className=''><a href='https://www.facebook.com/hajinprk' target='_blank' rel="noreferrer">Facebook</a></li>
                     <li className='' target='_blank'><a href='/'>Email</a></li>
                 </ul>
             </address>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,11 @@
 export default function NavBar() {
     return (
-        <section className='relative'>
-            <div className='absolute translate-x-2 translate-y-2 -z-10 h-20 w-full bg-green-800'>
+        <section className='relative group'>
+            <div className='transition-all delay-100 absolute translate-x-2 translate-y-2 group-hover:translate-y-4 group-hover:translate-x-4 -z-10 h-20 w-full bg-green-800'>
             </div>
-            <div className='absolute translate-x-4 translate-y-4 -z-20 h-20 w-full bg-green-900'>
+            <div className='transition-all absolute translate-x-4 translate-y-4 group-hover:translate-y-6 group-hover:translate-x-6 -z-20 h-20 w-full bg-green-900 shadow-lg'>
             </div>
-            <nav className='bg-green-300 h-20 w-full'>
+            <nav className='transition-all bg-green-300 h-20 w-full group-active:translate-x-1 group-active:translate-y-1'>
                 <ul className='h-full w-full flex items-center justify-around font-extrabold text-2xl'>
                     <li><a href='/'>About Me</a></li>
                     <li><a href='/'>Projects</a></li>
