59303fba9366ec2c8f8a01c70f38b26f4ecdabdc
Hajin Park <hajin1819@gmail.com>
2022-10-28 18:21:18 -0700

    Added dark mode color scheme

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
 export default function Footer() {
     return (
         <section className='group relative -skew-x-[4deg]'>
-            <div className='transition-all delay-100 absolute -translate-x-2 translate-y-2 group-hover:translate-y-4 group-hover:-translate-x-4 -z-10 h-20 w-full bg-green-400'>
+            <div className='transition-all delay-100 absolute -translate-x-2 translate-y-2 group-hover:translate-y-4 group-hover:-translate-x-4 -z-10 h-20 w-full bg-pink-400 dark:bg-pink-700'>
             </div>
-            <div className='transition-all absolute -translate-x-4 translate-y-4 group-hover:translate-y-6 group-hover:-translate-x-6 -z-20 h-20 w-full bg-green-200 shadow-lg'>
+            <div className='transition-all absolute -translate-x-4 translate-y-4 group-hover:translate-y-6 group-hover:-translate-x-6 -z-20 h-20 w-full bg-pink-200 dark:bg-pink-900 shadow-lg shadow-pink-700/40 dark:shadow-pink-900/60'>
             </div>
-            <address id='footer' className='transition-all group-hover:-translate-x-1 group-hover:translate-y-1 group-active:-translate-x-2 group-active:translate-y-2 bg-green-900 h-20 w-full'>
-                <ul className='h-full w-full flex items-center justify-around font-extrabold text-2xl text-green-400 not-italic'>
-                    <li className='hover:underline active:text-green-200'><a href='https://github.com/hajin-park' target='_blank' rel="noreferrer">Github</a></li>
-                    <li className='hover:underline active:text-green-200'><a href='https://www.linkedin.com/in/hajinpark/' target='_blank' rel="noreferrer">LinkedIn</a></li>
-                    <li className='hover:underline active:text-green-200'><a href='https://www.facebook.com/hajinprk' target='_blank' rel="noreferrer">Facebook</a></li>
-                    <li className='hover:underline active:text-green-200' target='_blank'><a href='/'>Email</a></li>
+            <address id='footer' className='transition-all group-hover:-translate-x-1 group-hover:translate-y-1 group-active:-translate-x-2 group-active:translate-y-2 bg-pink-900 dark:bg-pink-200 h-20 w-full'>
+                <ul className='h-full w-full flex items-center justify-around font-extrabold text-2xl text-pink-400 dark:text-pink-700 not-italic'>
+                    <li className='hover:underline active:text-pink-200 dark:active:text-pink-900'><a href='https://github.com/hajin-park' target='_blank' rel="noreferrer">Github</a></li>
+                    <li className='hover:underline active:text-pink-200 dark:active:text-pink-900'><a href='https://www.linkedin.com/in/hajinpark/' target='_blank' rel="noreferrer">LinkedIn</a></li>
+                    <li className='hover:underline active:text-pink-200 dark:active:text-pink-900'><a href='https://www.facebook.com/hajinprk' target='_blank' rel="noreferrer">Facebook</a></li>
+                    <li className='hover:underline active:text-pink-200 dark:active:text-pink-900' target='_blank'><a href='/'>Email</a></li>
                 </ul>
             </address>
         </section>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,17 @@
 export default function NavBar() {
     return (
         <section className='relative group skew-x-[4deg]'>
-            <div className='transition-all delay-100 absolute translate-x-2 translate-y-2 group-hover:translate-y-4 group-hover:translate-x-4 -z-10 h-20 w-full bg-green-700'>
+            <div className='transition-all delay-100 absolute translate-x-2 translate-y-2 group-hover:translate-y-4 group-hover:translate-x-4 -z-10 h-20 w-full bg-green-400 dark:bg-green-700'>
             </div>
-            <div className='transition-all absolute translate-x-4 translate-y-4 group-hover:translate-y-6 group-hover:translate-x-6 -z-20 h-20 w-full bg-green-900 shadow-lg'>
+            <div className='transition-all absolute translate-x-4 translate-y-4 group-hover:translate-y-6 group-hover:translate-x-6 -z-20 h-20 w-full bg-green-200 dark:bg-green-900 shadow-lg shadow-green-700/40 dark:shadow-green-900/60'>
             </div>
-            <nav className='transition-all bg-green-200 h-20 w-full group-hover:translate-x-1 group-hover:translate-y-1 group-active:translate-x-2 group-active:translate-y-2'>
-                <ul className='h-full w-full flex items-center justify-around font-extrabold text-2xl text-green-700'>
-                    <li className='hover:underline active:text-green-900'><a href='#footer'>About Me</a></li>
-                    <li className='hover:underline active:text-green-900'><a href='#footer'>Projects</a></li>
-                    <li className='hover:underline active:text-green-900'><a href='#footer'>Skills</a></li>
-                    <li className='hover:underline active:text-green-900'><a href='#footer'>Certificates</a></li>
-                    <li className='hover:underline active:text-green-900'><a href='#footer'>Socials</a></li>
+            <nav className='transition-all bg-green-900 dark:bg-green-200 h-20 w-full group-hover:translate-x-1 group-hover:translate-y-1 group-active:translate-x-2 group-active:translate-y-2'>   
+                <ul className='h-full w-full flex items-center justify-around font-extrabold text-2xl text-green-400 dark:text-green-700'>
+                    <li className='hover:underline active:text-green-200 dark:active:text-green-900'><a href='#footer'>About Me</a></li>
+                    <li className='hover:underline active:text-green-200  dark:active:text-green-900'><a href='#footer'>Projects</a></li>
+                    <li className='hover:underline active:text-green-200  dark:active:text-green-900'><a href='#footer'>Skills</a></li>
+                    <li className='hover:underline active:text-green-200  dark:active:text-green-900'><a href='#footer'>Certificates</a></li>
+                    <li className='hover:underline active:text-green-200  dark:active:text-green-900'><a href='#footer'>Socials</a></li>
                 </ul>
             </nav>
         </section>
