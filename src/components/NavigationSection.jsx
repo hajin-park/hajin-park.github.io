eda71b0e06168ec9e82cc67362838e0400e6bc29
Hajin Park <hajin1819@gmail.com>
2022-11-11 22:29:35 -0800

    Added navigation hover effect

--- src/components/NavigationSection.jsx
+++ src/components/NavigationSection.jsx
@@ -1,27 +1,38 @@
 export default function NavigationSection() {
     return (
         <nav className="relative w-full h-full">
-            <ul className="grid grid-cols-[1fr_1fr_240px] grid-rows-[35%_65%] w-full h-full overflow-hidden">
-                <li className="col-start-1 col-end-2 row-start-1 row-end-2 w-fit h-fit pt-14 pl-12">
-                    <a href="/" className="font-black text-5xl text-[#202020]">PROJECTS</a>
-                </li>
-                <li className="col-start-1 col-end-2 row-start-2 row-end-3 justify-self-end pt-12 pr-20 w-fit h-fit">
-                    <a href="/" className="font-black text-5xl text-[#202020]">SKILLS</a>
-                </li>
-                <li className="col-start-2 col-end-3 row-start-1 row-end-2 pl-20 w-fit h-fit">
-                    <a href="/" className="leading-[0.6] font-black text-5xl text-[#202020]">ABOUT ME</a>
-                </li>
-                <li className="col-start-3 col-end-4 row-span-2 justify-self-end pt-[6.5rem] w-fit h-fit">
-                    <a href="/" className="font-black text-5xl text-[#202020]">SOCIALS</a>
-                </li>
+            <ul className="grid grid-cols-[4fr_3fr_2fr] grid-rows-[35%_65%] w-full h-full overflow-hidden">
+
+                {/* Horizontal and vertical lines */}
                 <li className="overflow-hidden col-start-1 col-end-3 row-start-1 row-end-2">
                     <div className="ml-12 w-full h-full border-b border-black"></div>
+                </li>
+                <li className="col-start-3 col-end-4 row-start-1 row-end-2">
+                    <div className="w-2/5 h-full border-b border-black"></div>
                 </li>
                 <li className="col-start-1 col-end-2 row-start-1 row-end-3">
                     <div className="w-full h-full border-r border-black"></div>
                 </li>
                 <li className="col-start-2 col-end-3 row-start-1 row-end-3">
-                    <div className="w-4/5 h-2/3 border-r border-black"></div>
+                    <div className="w-full h-2/3 border-r border-black"></div>
+                </li>
+
+                {/* Hover effect blocks and navigation links*/}
+                <li className="overflow-hidden relative col-start-1 col-end-2 row-start-1 row-end-2">
+                    <a href="/" className="peer z-10 transition-all absolute top-16 left-12 font-black text-5xl text-[#303030] hover:text-black">PROJECTS</a>
+                    <div className="transition-all duration-700 bg-[#101010] opacity-30 translate-x-full peer-hover:translate-x-0 w-full h-full ml-12"></div>
+                </li>
+                <li className="overflow-hidden relative col-start-2 col-end-3 row-start-1 row-end-2">
+                    <a href="/" className="peer z-10 transition-all absolute -top-2 font-black text-5xl text-[#303030] hover:text-black">ABOUT ME</a>
+                    <div className="transition-all duration-700 bg-[#101010] opacity-30 -translate-x-full peer-hover:translate-x-0 w-full h-full"></div>
+                </li>
+                <li className="overflow-hidden relative col-start-1 col-end-2 row-start-2 row-end-3">
+                    <a href="/" className="peer z-10 transition-all absolute top-24 right-16 font-black text-5xl text-[#303030] hover:text-black">SKILLS</a>
+                    <div className="transition-all duration-700 bg-[#101010] opacity-30 translate-x-full peer-hover:translate-x-0 w-full h-full ml-12"></div>
+                </li>
+                <li className="overflow-hidden relative col-start-2 col-end-3 row-start-2 row-end-3">
+                    <a href="/" className="peer z-10 transition-all absolute top-9 left-48 font-black text-5xl text-[#303030] hover:text-black">EXPERIENCE</a>
+                    <div className="transition-all duration-700 bg-[#101010] opacity-30 -translate-x-full peer-hover:translate-x-0 w-full h-1/2"></div>
                 </li>
             </ul>
         </nav>
