557e5979727e91a87211c509eb983930f4e78850
Hajin Park <hajin1819@gmail.com>
2022-12-11 01:39:36 -0800

    Fixed height bug

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import LandingSection from './components/LandingSection'
 
 export default function App() {
     return (
-        <div className="relative h-[450%] lg:h-[425%] w-full overflow-hidden bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-amber-100 via-rose-200 to-sky-300">
+        <div className="relative w-full overflow-hidden bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-amber-100 via-rose-200 to-sky-300">
             <div className="z-10 absolute inset-0 bg-[url(https://grainy-gradients.vercel.app/noise.svg)] opacity-75 brightness-100 contrast-100"></div>
             <LandingSection />
         </div>
