4531e66d32c58793d789cc4bbb489b8434cc7261
Hajin Park <hajin1819@gmail.com>
2022-12-15 00:41:47 -0800

    Refactored project components

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,46 @@
 import ProjectCard from './shared/ProjectCard'
 
 export default function Projects() {
+    const projects = [
+        {
+            id: 1,
+            name: "Ucentials.org",
+            img: "ucentials_project.png"
+        },
+        {
+            id: 2,
+            name: "StockState.org",
+            img: "stockstate_project.png"
+        },
+        {
+            id: 3,
+            name: "OpenCV Game Bot",
+            img: "opencv_project.png"
+        },
+        {
+            id: 4,
+            name: "Instagram Tracker",
+            img: "instagram_project.png"
+        },
+        {
+            id: 5,
+            name: "Pygame Game",
+            img: "pygame_project.png"
+        },
+        {
+            id: 6,
+            name: "Discord Bot",
+            img: "discord_project.png"
+        },
+    ]
+
     return (
         <article className="grid w-full justify-center content-center mb-32">
             <h2 className="fade opacity-0 leading-tight bg-gradient-to-r bg-clip-text text-transparent text-center font-black text-7xl from-sky-400 to-rose-400 lg:text-9xl lg:from-sky-400 lg:to-rose-500 lg:leading-tight">
                 Projects
             </h2>
             <section className="grid grid-cols-1 lg:grid-cols-2 w-screen px-8 py-8 gap-8 lg:px-16 lg:py-16 lg:gap-16">
-                <ProjectCard name="Marketplace Website"/>
-                <ProjectCard name="Grocery Data Scraper"/>
-                <ProjectCard name="OpenCV Game Bot"/>
-                <ProjectCard name="Instagram Tracker"/>
-                <ProjectCard name="Pygame Game"/>
-                <ProjectCard name="Discord Bot"/>
+                {projects.map(project => <ProjectCard key={project.id} data={project} />)}
             </section>
         </article>
     )
--- src/components/shared/ProjectCard.jsx
+++ src/components/shared/ProjectCard.jsx
@@ -1,8 +1,11 @@
-export default function ProjectCard({ name }) {
+export default function ProjectCard({ data }) {
+    const {id, name, img} = data
+
     return (
         <section>
             <p className="font-bold text-3xl lg:text-4xl text-center">{name}</p>
-            <div className="w-full h-48 lg:h-96 bg-rose-100 rounded-xl">
+            <div className="w-full h-48 lg:h-96 bg-rose-100">
+                {`${img} ${id}`} 
             </div>
         </section>
     )
