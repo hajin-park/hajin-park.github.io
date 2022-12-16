04f44fd426d3018b5110797b654cdb2a5ad61efb
Hajin Park <hajin1819@gmail.com>
2022-12-15 17:48:44 -0800

    Dynamic background gradient colors - not working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useRef, useEffect } from 'react'
 import Header from './components/Header'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
@@ -6,12 +6,28 @@ import Contacts from './components/Contacts'
 import Backgrounds from './components/Backgrounds'
 
 export default function App() {
-    const callback = (entries) => entries.forEach(
-        entry => {
+    const gradients = useRef([0,100,200])
+
+    const callback = (entries) => {
+        entries.forEach(entry => {
             entry.isIntersecting 
                 ? entry.target.classList.add("animate-fadeIn")
                 : entry.target.classList.remove("animate-fadeIn")
-        })
+            }
+        )
+        backgroundTransition()
+    }
+
+    const backgroundTransition = () => {
+        const background = document.querySelector(".background")
+
+        background.classList.remove(`from-sky-${gradients.current[0]}`, `via-rose-${gradients.current[1]}`, `to-amber-${gradients.current[2]}`)
+
+        gradients.current = gradients.current.map(gradient => gradient === 900 ? 0 : gradient + 100)
+
+        background.classList.add(`from-sky-${gradients.current[0]}`, `via-rose-${gradients.current[1]}`, `to-amber-${gradients.current[2]}`)
+        console.log(gradients.current)
+    }
 
     useEffect(() => {
         const targets = document.querySelectorAll(".fade");
@@ -20,7 +36,7 @@ export default function App() {
     }, [])
 
     return (
-        <div className="relative w-full overflow-hidden bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-amber-100 via-rose-200 to-sky-300">
+        <div className="background relative w-full overflow-hidden bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-sky-0 via-rose-100 to-amber-200">
             <Backgrounds />
             <Header />
             <Projects />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,6 @@ export default function Header() {
             <h2 className="fade opacity-0 bg-gradient-to-r bg-clip-text text-transparent text-center font-black text-7xl from-sky-500 to-rose-400 lg:text-9xl lg:from-sky-400 lg:to-rose-300 leading-tight lg:leading-tight">
                 Hajin Park
             </h2>
-            <section className="grid grid-cols-2 pl-12 lg:pl-20">
-                <h3 className="fade opacity-0 border-r-2 text-right bg-gradient-to-r bg-clip-text text-transparent font-black text-3xl from-sky-500 to-rose-400 lg:text-6xl lg:from-sky-400 lg:via-sky-400 lg:to-rose-300">
-                    <p>Computer Vision</p>
-                    <p>Web Dev</p>
-                </h3>
-                <h3 className="fade opacity-0 text-left bg-gradient-to-r bg-clip-text text-transparent font-black text-4xl from-rose-400 to-rose-500 lg:text-6xl lg:from-rose-300 lg:to-rose-400">
-                    <p>Music</p>
-                    <p>Art</p>
-                </h3>
-            </section>
         </article>
     )
 }
