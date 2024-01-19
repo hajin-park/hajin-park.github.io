c5da0a86558a24c645c76d180228cedb9acf1dd2
Hajin Park <hajin1819@gmail.com>
2024-01-19 11:42:31 -0800

    ew

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-    return (
-        <article className="grid w-full h-screen justify-center content-center mb-32">
-            <h2 className="fade opacity-0 bg-gradient-to-r bg-clip-text text-transparent text-center font-black text-7xl from-sky-500 to-rose-400 lg:text-9xl lg:from-sky-400 lg:to-rose-300 leading-tight lg:leading-tight">
-                Hajin Park
-            </h2>
-        </article>
-    )
-}
